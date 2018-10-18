9414ad173b90ea53b13b1f32e1f1c6e1351fd43a
Md Nur <mdnur701@gmail.com>
2018-10-18 22:58:24 +0600

    Init porject with migration file and Model and Controller Resource

---
+++ database/migrations/2018_10_18_163344_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('category_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('description');
+            $table->text('content');
+            $table->enum('status', ['published','draft']);
+            $table->unsignedInteger('featured');
+            $table->timestamp('published_at');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
